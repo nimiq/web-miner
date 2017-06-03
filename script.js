59cec4b177177d3bf64929e0ea0e77514ab1c0ac
pb <berrang@cs.uni-saarland.de>
2017-06-03 16:08:13 -0600

    allow to load staging miner

--- script.js
+++ script.js
@@ -13,14 +13,36 @@ function isSupportedBrowser() {
 
 if (isSupportedBrowser()) {
     var scripts = ['geoip.js', 'map.js', 'miner.js'];
-    // Load main script.
-    var head = document.getElementsByTagName('head')[0];
-    for (var i = 0; i < scripts.length; ++i) {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = scripts[i];
-        head.appendChild(script);
+
+    // allow to load staging branch instead
+    if (window.location.hash === '#staging') {
+        var nimiq = 'https://cdn.nimiq-network.com/staging/nimiq.js';
+    } else {
+        var nimiq = 'https://cdn.nimiq-network.com/dist/nimiq.js';
     }
+
+    window.nimiq_loaded = false;
+    var head = document.getElementsByTagName('head')[0];
+
+    var ret = function() {
+        // Load main script.
+        if (!window.nimiq_loaded) {
+            window.nimiq_loaded = true;
+            for (var i = 0; i < scripts.length; ++i) {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = scripts[i];
+                head.appendChild(script);
+            }
+        }
+    }
+
+    var script = document.createElement('script');
+    script.onreadystatechange = ret;
+    script.onload = ret;
+    script.type = 'text/javascript';
+    script.src = nimiq;
+    head.appendChild(script);
 } else {
     document.getElementById('warning-old-browser').style.display = 'block';
 }
