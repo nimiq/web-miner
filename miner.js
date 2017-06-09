e2a81af74fbc4c0d1f12cb28bc6145fad789e1f0
danimoh <github.danimoh@neverbox.com>
2017-06-09 17:09:38 -0600

    distinguish between screen orientation change and keyboard open

--- miner.js
+++ miner.js
@@ -589,6 +589,18 @@ Nimiq.init($ => {
 });
 
 
+function checkScreenOrientation() {
+    // we check the screen dimensions instead of innerWidth/innerHeight for correct behaviour when the keyboard
+    // is shown on mobile
+    var isLandscape = window.screen.width >= window.screen.height;
+    if (isLandscape && window.innerHeight < 400) {
+        document.body.classList.add('mobile-landscape');
+    } else {
+        document.body.classList.remove('mobile-landscape');
+    }
+}
+window.addEventListener('resize', checkScreenOrientation);
+checkScreenOrientation();
 
 
 
