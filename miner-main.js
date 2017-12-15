dbed1aab34d33f433f5fccf2e538beee8f9e39eb
danimoh <github.danimoh@neverbox.com>
2017-12-14 19:16:22 -0600

    be more tolerant in mobile landscape detection

--- miner-main.js
+++ miner-main.js
@@ -483,7 +483,7 @@ function checkScreenOrientation() {
     // we check the screen dimensions instead of innerWidth/innerHeight for correct behaviour when the keyboard
     // is shown on mobile
     var isLandscape = window.screen.width >= window.screen.height;
-    if (isLandscape && window.innerHeight < 540) {
+    if (isLandscape && window.innerHeight < 480) {
         document.body.classList.add('mobile-landscape');
     } else {
         document.body.classList.remove('mobile-landscape');
